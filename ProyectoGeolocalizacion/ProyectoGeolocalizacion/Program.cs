e4e412825adaf0d825b9586842b60c0a6ed51f94
Jona <Jona@JONAVAVIO>
2019-07-01 12:57:27 +0200

    Comentario de prueba para rama

--- ProyectoGeolocalizacion/ProyectoGeolocalizacion/Program.cs
+++ ProyectoGeolocalizacion/ProyectoGeolocalizacion/Program.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Logging;
 
 namespace ProyectoGeolocalizacion
 {
+    // prueba comentario rama nueva
     public class Program
     {
         public static void Main(string[] args)
