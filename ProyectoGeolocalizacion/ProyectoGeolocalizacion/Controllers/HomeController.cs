11f6f10b21ad0bafda94c6f0c5dae137a373fc35
Iciar Etxebarria Martín <iciar.etxebarria@gmail.com>
2019-07-03 18:24:47 +0200

    3 de julio pagina de inicio

--- ProyectoGeolocalizacion/ProyectoGeolocalizacion/Controllers/HomeController.cs
+++ ProyectoGeolocalizacion/ProyectoGeolocalizacion/Controllers/HomeController.cs
@@ -5,18 +5,69 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ProyectoGeolocalizacion.Models;
+using ProyectoGeolocalizacion.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ProyectoGeolocalizacion.Controllers
 {
     public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            //hola ramaIciar
 
-            //iciar
-            return View();
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
         }
+
+
+        public async Task<IActionResult> Index(string alias)
+        {
+            ViewData["Viewdata"] = false;
+
+            if (alias != null)
+            {
+                ViewData["Viewdata"] =await AliasExistsAsync(alias);
+                ViewData["alias"] = alias;
+                return View();
+
+            }
+            else
+            {
+                return View();
+
+            }
+
+
+
+
+        }
+
+        //COMPROBAR ALIAS
+        public async Task<bool> AliasExistsAsync(string alias)
+        {
+            var device = await _context.Device.FirstOrDefaultAsync(m => m.Alias == alias);
+
+            if (device == null)
+            {
+
+                Device newDevice = new Device
+                {
+                    Alias = alias
+                };
+                _context.Add(newDevice);
+                await _context.SaveChangesAsync();
+                return false;
+
+            }
+            else
+            {
+
+                return true;
+            }
+        }
+      
 
         public IActionResult Privacy()
         {
--- ProyectoGeolocalizacion/ProyectoGeolocalizacion/Data/ApplicationDbContext.cs
+++ ProyectoGeolocalizacion/ProyectoGeolocalizacion/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ProyectoGeolocalizacion.Models;
 
 namespace ProyectoGeolocalizacion.Data
 {
@@ -12,5 +13,6 @@ namespace ProyectoGeolocalizacion.Data
             : base(options)
         {
         }
+        public DbSet<ProyectoGeolocalizacion.Models.Device> Device { get; set; }
     }
 }
