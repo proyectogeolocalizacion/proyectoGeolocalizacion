c33b89470809d88ff0f3ce5c0b6fef330d1641dc
Iciar Etxebarria Martín <iciar.etxebarria@gmail.com>
2019-07-04 17:06:31 +0200

    ,

---
+++ ProyectoGeolocalizacion/ProyectoGeolocalizacion/Data/Migrations/20190703101510_InitialCreateMaps.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProyectoGeolocalizacion.Data.Migrations
+{
+    public partial class InitialCreateMaps : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- ProyectoGeolocalizacion/ProyectoGeolocalizacion/Hubs/Mapa.cs
+++ ProyectoGeolocalizacion/ProyectoGeolocalizacion/Hubs/Mapa.cs
@@ -1,14 +1,28 @@
+
 ﻿using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System;
+using Microsoft.AspNetCore.SignalR;
+﻿using System;
+using Microsoft.AspNetCore.SignalR;
+
 
 namespace ProyectoGeolocalizacion.Hubs
 {
     public class Mapa : Hub
     {
 
+
+
+
+
+        
+
+
+        
 
     }
 }
