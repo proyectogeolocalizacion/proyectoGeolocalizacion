b261a5969bf5395a2a2a6c3e19185be6f2ee4878
Iciar Etxebarria Martín <iciar.etxebarria@gmail.com>
2019-07-05 16:30:15 +0200

    hgjhghj

--- ProyectoGeolocalizacion/ProyectoGeolocalizacion/wwwroot/js/site.js
+++ ProyectoGeolocalizacion/ProyectoGeolocalizacion/wwwroot/js/site.js
@@ -34,7 +34,7 @@ function initMap() {
             handleLocationError(true, infoWindow, map.getCenter());
         });
     } else {
-        // Browser doesn't support Geolocation
+         Browser doesn't support Geolocation
         handleLocationError(false, infoWindow, map.getCenter());
     }
 
@@ -55,38 +55,38 @@ function initMap() {
 
 //var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+//var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1IjoibWlubmFoZWkiLCJhIjoiY2p4a2w5eDV1MjlrZzN6bno4YndzcGoycyJ9.t6dIk600zRcR4wHtWNZH_Q'
-}).addTo(mymap);
+//L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+//    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//    maxZoom: 18,
+//    id: 'mapbox.streets',
+//    accessToken: 'pk.eyJ1IjoibWlubmFoZWkiLCJhIjoiY2p4a2w5eDV1MjlrZzN6bno4YndzcGoycyJ9.t6dIk600zRcR4wHtWNZH_Q'
+//}).addTo(mymap);
 
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
-
-
+//var marker = L.marker([51.5, -0.09]).addTo(mymap);
 
 
- mymap.locate({setView: true, watch: false}) /* This will return map so you can do chaining, follow where you go */
-        .on('locationfound', function(e){
-            var marker = L.marker([e.latitude, e.longitude]).bindPopup('Your are here :)');
-            var circle = L.circle([e.latitude, e.longitude], e.accuracy/2, {
-                weight: 1,
-                color: 'blue',
-                fillColor: '#cacaca',
-                fillOpacity: 0.2
-            });
-            mymap.addLayer(marker);
-            mymap.addLayer(circle);
-        })
-       .on('locationerror', function(e){
-            console.log(e);
-            alert("Location access denied.");
-     });
 
-//loadMap('mymap');
+
+// mymap.locate({setView: true, watch: false}) /* This will return map so you can do chaining, follow where you go */
+//        .on('locationfound', function(e){
+//            var marker = L.marker([e.latitude, e.longitude]).bindPopup('Your are here :)');
+//            var circle = L.circle([e.latitude, e.longitude], e.accuracy/2, {
+//                weight: 1,
+//                color: 'blue',
+//                fillColor: '#cacaca',
+//                fillOpacity: 0.2
+//            });
+//            mymap.addLayer(marker);
+//            mymap.addLayer(circle);
+//        })
+//       .on('locationerror', function(e){
+//            console.log(e);
+//            alert("Location access denied.");
+//     });
+
+////loadMap('mymap');
 
 
 
