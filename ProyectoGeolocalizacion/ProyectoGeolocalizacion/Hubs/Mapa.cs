68356c717335225c4f69e857fe681ba5e161a947
Jona <Jona@JONAVAVIO>
2019-07-10 15:56:56 +0200

    ......arreglos

--- ProyectoGeolocalizacion/ProyectoGeolocalizacion/Hubs/Mapa.cs
+++ ProyectoGeolocalizacion/ProyectoGeolocalizacion/Hubs/Mapa.cs
@@ -22,7 +22,7 @@ namespace ProyectoGeolocalizacion.Hubs
         public async Task SendMessage(double longitude, double latitude, string alias)
         {
 
-            await Clients.All.SendAsync("ReceiveMessage", longitude, latitude);
+            await Clients.All.SendAsync("ReceiveMessage", longitude, latitude, alias);
 
             Location location = new Location();
             Device device = await _context.Device.FirstOrDefaultAsync(x=>x.Alias == alias);
@@ -30,11 +30,12 @@ namespace ProyectoGeolocalizacion.Hubs
             //location.DeviceId = 1;
             location.Latitude = latitude;
             location.Longitude = longitude;
+            location.Time = DateTime.Now;
             
             _context.Add(location);
             await _context.SaveChangesAsync();
-            var longitud = await _context.Location.Include(x => x.Longitude).FirstOrDefaultAsync();
-            var latitud = await _context.Location.Include(x => x.Latitude).FirstOrDefaultAsync();
+            //var longitud = await _context.Location.Include(x => x.Longitude).FirstOrDefaultAsync();
+            //var latitud = await _context.Location.Include(x => x.Latitude).FirstOrDefaultAsync();
         }
 
     }
