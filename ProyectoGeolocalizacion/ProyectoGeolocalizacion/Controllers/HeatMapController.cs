8d7a6c8b695669ef09ecc842c4a56c3b99ae0d6f
Iciar Etxebarria Martín <iciar.etxebarria@gmail.com>
2019-07-10 09:57:30 +0200

    actualizacion iciar

--- ProyectoGeolocalizacion/ProyectoGeolocalizacion/Controllers/HeatMapController.cs
+++ ProyectoGeolocalizacion/ProyectoGeolocalizacion/Controllers/HeatMapController.cs
@@ -4,13 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace ProyectoGeolocalizacion.Controllers
 {
-    public class MapRuta : Controller
+    public class HeatMapController : Controller
     {
-        // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
