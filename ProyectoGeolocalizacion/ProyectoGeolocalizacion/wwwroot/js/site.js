3397ebf6e4a70a77cef58af0a4f1a6c0ae7bda13
Iciar Etxebarria Martín <iciar.etxebarria@gmail.com>
2019-07-12 15:34:15 +0200

    fusion con rama jonatan (mapa rutas)

--- ProyectoGeolocalizacion/ProyectoGeolocalizacion/wwwroot/js/site.js
+++ ProyectoGeolocalizacion/ProyectoGeolocalizacion/wwwroot/js/site.js
@@ -1,94 +1,34 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿
 
 
+var map = L.Wrld.map("routemap", "91a68d505577cb772e2ba97464e831bf", {
+    center: [43.2630126, -2.9349852],
+    zoom: 8
+});
 
+var polylinePoints = [
+    [43.2630126, -2.9349852],
+    [43.260012, -2.9204827],
+    [43.261212, -2.9304827],
+    [43.263512, -2.9395127]
+];
 
-//var x = document.getElementById("jonatan");
+var polyline = L.Wrld.polyline(polylinePoints, { color: '#3388ff', weight: 3 }).addTo(map);
+map.fitBounds(polyline.getBounds());
 
-//function getLocation() {
-//    if (navigator.geolocation) {
-//        navigator.geolocation.getCurrentPosition(showPosition);
-//    }
-//    else {
-//        x.innerHTML = "Geolocation is not supported by this browser.";
-//    }
-//}
+//..........................Calendario busqueda fecha maproutes...............................//
 
-//function showPosition(position) {
-//    x.innerHTML = "Latitude: " + position.coords.latitude +
-//        "<br>Longitude: " + position.coords.longitude;
-//    console.log(position)
-//}
-
-//var mymap = L.map('mapid').setView([43.2630126, -2.9349852], 10);
-
-//L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-//    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//    maxZoom: 18,
-//    id: 'mapbox.streets',
-//    accessToken: 'pk.eyJ1IjoibWlubmFoZWkiLCJhIjoiY2p4a2w5eDV1MjlrZzN6bno4YndzcGoycyJ9.t6dIk600zRcR4wHtWNZH_Q'
-//}).addTo(mymap);
-
-
-
-//mymap.locate({ setView: true, watch: false }) /* This will return map so you can do chaining */
-//    .on('locationfound', function (e) {
-//              connection.invoke("SendMessage", e.longitude, e.latitude).catch(function (err) {
-//              return console.error(err.toString());
-
-
-
-//        var marker = L.marker([e.latitude, e.longitude]).bindPopup('Your are here :)');
-//        var circle = L.circle([e.latitude, e.longitude], e.accuracy / 2, {
-//            weight: 1,
-//            color: 'green',
-//            fillColor: '#d620d3',
-//            fillOpacity: 0.1
-//        });
-//        mymap.addLayer(marker);
-//        mymap.addLayer(circle);
-//        console.log(e.latitude);
-//    })
-
-//    .on('locationerror', function (e) {
-//        console.log(e);
-//        alert("Location access denied.");
-//    });
-
-
-
-
-
-
-
-//var map = L.map('mapid').setView([43.2630126, -2.9349852], 10);
-
-//function onAccuratePositionProgress(e) {
-//    console.log(e.accuracy);
-//    console.log(e.latlng);
-//}
-
-//function onAccuratePositionFound(e) {
-//    console.log(e.accuracy);
-//    console.log(e.latlng);
-//}
-
-//function onAccuratePositionError(e) {
-//    console.log(e.message)
-//}
-
-//map.on('accuratepositionprogress', onAccuratePositionProgress);
-//map.on('accuratepositionfound', onAccuratePositionFound);
-//map.on('accuratepositionerror', onAccuratePositionError);
-
-//map.findAccuratePosition({
-//    maxWait: 15000, // defaults to 10000
-//    desiredAccuracy: 30 // defaults to 20
-//});
-
+$(document).ready(function () {
+    var date_input = $('input[name="date"]'); //our date input has the name "date"
+    var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
+    var options = {
+        format: 'mm/dd/yyyy',
+        container: container,
+        todayHighlight: true,
+        autoclose: true,
+    };
+    date_input.datepicker(options);
+})
 
 
 
