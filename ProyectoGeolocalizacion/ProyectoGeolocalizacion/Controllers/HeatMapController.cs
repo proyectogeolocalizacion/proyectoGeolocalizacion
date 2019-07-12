6fd875c35e89211356494b87c258cdac42dc91e9
Iciar Etxebarria Martín <iciar.etxebarria@gmail.com>
2019-07-12 10:13:32 +0200

    frwfw

--- ProyectoGeolocalizacion/ProyectoGeolocalizacion/Controllers/HeatMapController.cs
+++ ProyectoGeolocalizacion/ProyectoGeolocalizacion/Controllers/HeatMapController.cs
@@ -3,16 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProyectoGeolocalizacion.Data;
 
 namespace ProyectoGeolocalizacion.Controllers
 {
     public class HeatMapController : Controller
     {
-        public IActionResult Index(string alias, string canal)
+        private readonly ApplicationDbContext _context;
+
+        public HeatMapController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> IndexAsync(string alias, string canal)
         {
 
             ViewData["alias"] = alias;
             ViewData["canal"] = canal;
+
+            if (alias != null)
+            {
+                var device = await _context.Device.Where(x => x.Alias == alias).FirstOrDefaultAsync();
+                var locations = device.Locations.ToList();
+                var allLocations = await _context.Device.Include(x=>x.Locations).ToListAsync();
+            }
             return View();
         }
     }
