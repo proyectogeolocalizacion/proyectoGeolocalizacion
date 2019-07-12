6ca5e02acea8eb495a49ca9c88221b7d774bdb78
Jona <Jona@JONAVAVIO>
2019-07-12 17:34:19 +0200

    ....

--- ProyectoGeolocalizacion/ProyectoGeolocalizacion/Controllers/MapsRoutesController.cs
+++ ProyectoGeolocalizacion/ProyectoGeolocalizacion/Controllers/MapsRoutesController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProyectoGeolocalizacion.Data;
+using ProyectoGeolocalizacion.Models;
 
 namespace ProyectoGeolocalizacion.Controllers
 {
@@ -28,7 +29,7 @@ namespace ProyectoGeolocalizacion.Controllers
             }
 
 
-            var devices = await _context.Device.Where(x => x.Status == "Online").Where(x => x.Channel == canal).ToListAsync();
+            var devices = await _context.Device.Where(x => x.Channel == canal).ToListAsync();
             ViewData["alias"] = alias;
             ViewData["canal"] = canal;
             return View(devices);
@@ -57,6 +58,24 @@ namespace ProyectoGeolocalizacion.Controllers
 
 //public async Task<IActionResult> ShowRoute(string alias, string canal)
 //{
+//    ViewData["alias"] = alias;
+//    ViewData["canal"] = canal;
+
+//    if (alias != null)
+//    {
+//        var device = await _context.Device.Where(x => x.Alias == alias).FirstOrDefaultAsync();
+//        var channel = await _context.Channel.Where(x => x.Name == canal).FirstOrDefaultAsync();
+
+
+//        List<Location> locations = await _context.Location.Where(x => x.DeviceId == device.Id).ToListAsync();
+//        return View(locations);
+
+//    }
+//    var allLocations = await _context.Location.ToListAsync();
+//    return View(allLocations);
+
+
+
 
 
 //}
--- ProyectoGeolocalizacion/ProyectoGeolocalizacion/wwwroot/js/site.js
+++ ProyectoGeolocalizacion/ProyectoGeolocalizacion/wwwroot/js/site.js
@@ -12,11 +12,12 @@ var map = L.Wrld.map("routemap", "91a68d505577cb772e2ba97464e831bf", {
 var polylinePoints = [
     [43.2630126, -2.9349852],
     [43.260012, -2.9204827],
-    [43.261212, -2.9304827],
-    [43.263512, -2.9395127]
+    [43.291212, -3.9304827],
+    [43.2293512, -3.9495127]
+
 ];
 
-var polyline = L.Wrld.polyline(polylinePoints, { color: '#3388ff', weight: 3 }).addTo(map);
+var polyline = L.Wrld.polyline(polylinePoints, { color: 'black', weight: 3 }).addTo(map);
 map.fitBounds(polyline.getBounds());
 
 //..........................Calendario busqueda fecha maproutes...............................//
