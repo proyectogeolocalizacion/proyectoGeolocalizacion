6642068bf68a085806da7df64dd80533e306108a
Iciar Etxebarria Martín <iciar.etxebarria@gmail.com>
2019-07-15 10:03:38 +0200

    heatmap

--- ProyectoGeolocalizacion/ProyectoGeolocalizacion/Models/Location.cs
+++ ProyectoGeolocalizacion/ProyectoGeolocalizacion/Models/Location.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,7 +14,11 @@ namespace ProyectoGeolocalizacion.Models
     {
 
         public int Id { get; set; }
+
+       
         public double Latitude { get; set; }
+
+       
         public double Longitude { get; set; }
 
         public DateTime Time { get; set; }
--- ProyectoGeolocalizacion/ProyectoGeolocalizacion/wwwroot/js/leaflet-heatmap.js
+++ ProyectoGeolocalizacion/ProyectoGeolocalizacion/wwwroot/js/leaflet-heatmap.js
@@ -1,26 +1,28 @@
 ﻿
 
 
-var locations = document.getElementById("locations").childNodes;
+var locations = document.getElementById("locations").innerHTML;
 console.log(locations)
 
-//var obj = JSON.parse(locations); 
+var obj = JSON.parse(locations); 
 
 
 
 var testData = {
     max: 8,
-    data: [
-        { lat: 26.74, lng: -80.7728, count: 1 },
-        { lat: 26.75, lng: -80.55, count: 5 },
-        { lat: 26.76, lng: -80.55, count: 5 },
-        { lat: 26.77, lng: -80.55, count: 5 },
-        { lat: 26.78, lng: -80.55, count: 5 },
-        { lat: 26.765, lng: -80.55, count: 5 },
-        { lat: 26.74, lng: -80.55, count: 5 },
-        { lat: 26.79, lng: -80.55, count: 5 },
-        { lat: 26.80, lng: -80.55, count: 5 },
-    ]
+    data: obj
+
+    //    [
+    //    { lat: 26.74, lng: -80.7728, count: 1 },
+    //    { lat: 26.75, lng: -80.55, count: 5 },
+    //    { lat: 26.76, lng: -80.55, count: 5 },
+    //    { lat: 26.77, lng: -80.55, count: 5 },
+    //    { lat: 26.78, lng: -80.55, count: 5 },
+    //    { lat: 26.765, lng: -80.55, count: 5 },
+    //    { lat: 26.74, lng: -80.55, count: 5 },
+    //    { lat: 26.79, lng: -80.55, count: 5 },
+    //    { lat: 26.80, lng: -80.55, count: 5 },
+    //]
 };
 
 //var testData = {
@@ -68,8 +70,8 @@ var cfg = {
 var heatmapLayer = new HeatmapOverlay(cfg);
 
 var map = new L.Map('heatmap', {
-    center: new L.LatLng(25.6586, -80.3568),
-    zoom: 8,
+    center: new L.LatLng(43.2630126, -2.9349852),
+    zoom: 12,
     layers: [baseLayer, heatmapLayer]
 });
 
