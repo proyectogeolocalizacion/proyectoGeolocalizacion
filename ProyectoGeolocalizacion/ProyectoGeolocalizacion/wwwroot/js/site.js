27d0fc816bf6c45862a137983bb08613e394d03d
Iciar Etxebarria Martín <iciar.etxebarria@gmail.com>
2019-07-15 13:28:55 +0200

    cambios ultimos

--- ProyectoGeolocalizacion/ProyectoGeolocalizacion/wwwroot/js/site.js
+++ ProyectoGeolocalizacion/ProyectoGeolocalizacion/wwwroot/js/site.js
@@ -9,17 +9,20 @@ var map = L.Wrld.map("routemap", "91a68d505577cb772e2ba97464e831bf", {
 var polylinePoints = [
     [43.2630126, -2.9349852],
     [43.260012, -2.9204827],
-    [43.261212, -2.9304827],
-    [43.263512, -2.9395127]
+    [43.261512, -2.9204827],
+    [43.2619512, -2.92495127],
+    [43.2619512, -2.93495127],
+    [43.2635512, -2.93585127]
+
 ];
 
-var polyline = L.Wrld.polyline(polylinePoints, { color: '#3388ff', weight: 3 }).addTo(map);
+var polyline = L.Wrld.polyline(polylinePoints, { color: '#FF0000', weight: 4 }).addTo(map);
 map.fitBounds(polyline.getBounds());
 
 //..........................Calendario busqueda fecha maproutes...............................//
 
 $(document).ready(function () {
-    var date_input = $('input[name="date"]'); //our date input has the name "date"
+    var date_input = $('input[name="fecha"]'); //our date input has the name "date"
     var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
     var options = {
         format: 'mm/dd/yyyy',
