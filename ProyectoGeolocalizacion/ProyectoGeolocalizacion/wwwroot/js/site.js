1107efabc3d8a696430ba33f5ff6ab6c628238f8
Jona <Jona@JONAVAVIO>
2019-07-16 18:56:24 +0200

    minichanges

--- ProyectoGeolocalizacion/ProyectoGeolocalizacion/wwwroot/js/site.js
+++ ProyectoGeolocalizacion/ProyectoGeolocalizacion/wwwroot/js/site.js
@@ -8,21 +8,17 @@
 
 var map = L.Wrld.map("routemap", "91a68d505577cb772e2ba97464e831bf", {
     center: [43.2630126, -2.9349852],
-    zoom: 8
+    zoom: 15
 });
 
 var polylinePoints = [
     [43.2630126, -2.9349852],
-    [43.260012, -2.9204827],
-    [43.261512, -2.9204827],
-    [43.2619512, -2.92495127],
-    [43.2619512, -2.93495127],
-    [43.2635512, -2.93585127]
-
+    [43.2630127, -2.9349853]
+   
 ];
 
 var polyline = L.Wrld.polyline(polylinePoints, { color: '#FF0000', weight: 4 }).addTo(map);
-map.fitBounds(polyline.getBounds());
+//map.fitBounds(polyline.getBounds());
 
 
 
